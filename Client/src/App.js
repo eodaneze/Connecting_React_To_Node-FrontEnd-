28ff193f3752d23a9b4def0e014d6abdb349e1ff
Ezeali Daniel Okechukwu <ezealiokechukwu1999@gmail.com>
2022-11-08 06:05:52 -0800

    created a new backend folder with server.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
-import Main from './Main';
+import {useEffect,useState} from "react"
+import axios from 'axios'
 
 function App() {
+  const [apiResponse, setApiResponse] = useState("");
+
+  useEffect(() => {
+        // fetch('http://localhost:5000/testApi')
+        // .then(res=>res.text())
+        // .then(res=>setApiResponse)
+        axios.get('http://localhost:5000/testApi', (res, req)=> {
+             res.send(setApiResponse)
+        })
+        .then(res=>res.text)
+        
+  }, [])
   return (
     
     <div className="App">
-        
+        <p> {apiResponse}</p>
     </div>
   );
 }
